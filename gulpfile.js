fe1aba875f36464f43d861a4e7ec32afbab9721c
EdwinBanda <casemirobanda@gmail.com>
2024-02-04 21:56:03 +0200

    refactor: changing from 'import' to 'require' to be compatible with commonJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,81 @@
-import gulp from 'gulp'
-import babel from 'gulp-babel'
-import concat from 'gulp-concat'
+// import gulp from 'gulp'
+// import babel from 'gulp-babel'
+// import concat from 'gulp-concat'
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const {series} = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 function transpileIndex(){
     return gulp.src('node_modules/openai/index.mjs')
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/preset-env'],
-            comments: true
+            comments: false
         }))
         .pipe(concat('index.mjs'))
-        .pipe(gulp.dest('node_modules/openai/'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('/node_modules/openai/'))
 }
+
+
 
 function transpileCore(){
-    return gulp.src('node_modules/openai/core.mjs')
+    return gulp.src('node_modules/openai/core.js')
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/preset-env'],
             comments: true
         }))
-        .pipe(concat('core.mjs'))
+        .pipe(concat('core.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('node_modules/openai/'))
 }
 
-function transpilePag(){
-    return gulp.src('node_modules/openai/pagination.mjs')
-        .pipe(babel({
-            presets: ['@babel/preset-env'],
-            comments: true
-        }))
-        .pipe(concat('pagination.mjs'))
-        .pipe(gulp.dest('node_modules/openai/'))
-}
+// function transpilePag(){
+//     return gulp.src('node_modules/openai/pagination.mjs')
+//         .pipe(babel({
+//             presets: ['@babel/preset-env'],
+//             comments: true
+//         }))
+//         .pipe(concat('pagination.mjs'))
+//         .pipe(gulp.dest('node_modules/openai/'))
+// }
 
-function transpileError(){
-    return gulp.src('node_modules/openai/error.mjs')
-        .pipe(babel({
-            presets: ['@babel/preset-env'],
-            comments: true
-        }))
-        .pipe(concat('error.mjs'))
-        .pipe(gulp.dest('node_modules/openai/'))
-}
+// function transpileError(){
+//     return gulp.src('node_modules/openai/error.mjs')
+//         .pipe(babel({
+//             presets: ['@babel/preset-env'],
+//             comments: true
+//         }))
+//         .pipe(concat('error.mjs'))
+//         .pipe(gulp.dest('node_modules/openai/'))
+// }
 
-function transpileUploads(){
-    return gulp.src('node_modules/openai/uploads.mjs')
-        .pipe(babel({
-            presets: ['@babel/preset-env'],
-            comments: true
-        }))
-        .pipe(concat('uploads.mjs'))
-        .pipe(gulp.dest('node_modules/openai/'))
-}
+// function transpileUploads(){
+//     return gulp.src('node_modules/openai/uploads.mjs')
+//         .pipe(babel({
+//             presets: ['@babel/preset-env'],
+//             comments: true
+//         }))
+//         .pipe(concat('uploads.mjs'))
+//         .pipe(gulp.dest('node_modules/openai/'))
+// }
 
 function transpileStreaming(){
-    return gulp.src('node_modules/openai/streaming.mjs')
+    return gulp.src('node_modules/openai/streaming.js')
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/preset-env'],
             comments: true
         }))
-        .pipe(concat('streaming.mjs'))
+        .pipe(concat('streaming.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('node_modules/openai/'))
 }
 
-gulp.task('default', gulp.series(transpileCore,transpileError,transpilePag,transpileStreaming,transpileUploads,transpileIndex))
-// module.exports = series(transpileOpenAI)
+// gulp.task('default', gulp.series(traspileIndex))
+module.exports.default = series(transpileStreaming)
